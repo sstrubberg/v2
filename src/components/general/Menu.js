59c11952b0f28c86e2ab7394c2d425b3d007b5a1
Scott <sstrubberg@us.ibm.com>
2020-04-10 11:02:23 -0500

    understanding useState and toggle function more than ever

--- src/components/general/Menu.js
+++ src/components/general/Menu.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import styled from '@emotion/styled'
+import { motion } from 'framer-motion'
 
 const Header = styled.header`
   font-family: ${props => props.theme.fonts.body};
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  /* max-height: ${props => (props.open ? '100%' : '60px')}; */
 `
 
 const Nav = styled.nav`
@@ -12,11 +20,6 @@ const Nav = styled.nav`
   align-items: center;
   height: 100%;
   width: 100%;
-  margin: 0 auto;
-  padding-top: 0;
-  padding-bottom: 0;
-  padding-right: 1.5rem;
-  padding-left: 1.5rem;
   @media screen and (min-width: ${props => props.theme.responsive.md}) {
     padding-top: 0;
     padding-bottom: 0;
@@ -26,9 +29,21 @@ const Nav = styled.nav`
   }
 `
 
-const UnOrderedList = styled.ul``
+const UnOrderedList = styled.ul`
+  position: relative;
+  @media screen and (min-width: ${props => props.theme.responsive.md}) {
+    padding: 0;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-gap: 0.75rem;
+    justify-items: end;
+  }
+`
 
-const ListItem = styled.li``
+const ListItem = styled(motion.li)`
+  display: flex;
+  justify-content: center;
+`
 
 const Toggle = styled.button`
   cursor: pointer;
@@ -43,23 +58,44 @@ const Toggle = styled.button`
   }
 `
 
-const Menu = () => (
-  <Header>
-    <Nav>
-      <Toggle></Toggle>
-      <UnOrderedList>
-        <ListItem>
-          <Link to="/">Scott Strubberg</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/portfolio">Portfolio</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/contact">Contact</Link>
-        </ListItem>
-      </UnOrderedList>
-    </Nav>
-  </Header>
-)
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function toggle() {
+    setIsOpen(!isOpen)
+  }
+
+  const listItemVariants = {
+    open: {
+      opacity: 1,
+    },
+    closed: {
+      opacity: 0,
+    },
+  }
+
+  return (
+    <Header>
+      <Nav>
+        <Toggle onClick={toggle} aria-label="Toggle Menu"></Toggle>
+        <UnOrderedList>
+          <ListItem
+            initial={false}
+            variants={listItemVariants}
+            animate={isOpen ? 'open' : 'closed'}
+          >
+            <Link to="/">Scott Strubberg</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/portfolio">Portfolio</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/contact">Contact</Link>
+          </ListItem>
+        </UnOrderedList>
+      </Nav>
+    </Header>
+  )
+}
 
 export default Menu

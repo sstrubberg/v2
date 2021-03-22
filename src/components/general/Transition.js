c1b0d0d2b38e23a90671ba65f3c2f75eb2bdebb4
Scott Strubberg <sstrubberg@us.ibm.com>
2021-03-21 19:51:23 -0500

    feat: page transitions set-need to update framer

--- src/components/general/Transition.js
+++ src/components/general/Transition.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
-const Transition = ({ children }) => {
-  const variants = {}
-  return <div>{children}</div>
+const Transition = ({ children, location }) => {
+  const variants = {
+    initial: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+      transition: {
+        duration: 0.35,
+        delay: 0.35,
+        when: 'beforeChildren',
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 0.35,
+      },
+    },
+  }
+  return (
+    <AnimatePresence>
+      <motion.main
+        key={location.pathname}
+        variants={variants}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        id="main"
+      >
+        {children}
+      </motion.main>
+    </AnimatePresence>
+  )
 }
 
 export default Transition

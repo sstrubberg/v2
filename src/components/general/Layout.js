f6c3a5613b0634a851601f81b3392725b5bcd4c4
Scott Strubberg <sstrubberg@us.ibm.com>
2021-03-23 12:04:54 -0500

    feat: seo rdy

--- src/components/general/Layout.js
+++ src/components/general/Layout.js
@@ -21,10 +21,7 @@ const Layout = props => {
   return (
     <>
       <Global styles={globalStyles} />
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
+      <Helmet />
       <Menu />
       <Transition {...props}>
         <Root>{children}</Root>
--- src/components/general/SEO.js
+++ src/components/general/SEO.js
@@ -1,1 +1,50 @@
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const SEO = ({ title, description, image }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      {
+        site {
+          siteMetadata {
+            author
+            description
+            github
+            linkedIn
+            siteUrl
+            title
+            image
+          }
+        }
+      }
+    `
+  )
+
+  const metaTitle = title || site.siteMetadata.title
+  const metaDescription = description || site.siteMetadata.description
+  const metaImage = image || site.siteMetadata.url + site.siteMetadata.image
+
+  return (
+    <Helmet
+      defaultTitle={site.siteMetadata.title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+    >
+      <html lang="en" />
+      <title>{title}</title>
+
+      {/* Meta Tags */}
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={site.siteMetadata.description} />
+      <meta name="image" content={image} />
+
+      {/* Open Graph */}
+      <meta property="og:image" content={metaImage} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+    </Helmet>
+  )
+}
+
+export default SEO

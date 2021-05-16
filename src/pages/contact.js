942ff13666ea488d7667eb2c14508f65ca180bdf
Scott Strubberg <sstrubberg@us.ibm.com>
2021-05-16 09:28:09 -0500

    feat(contact): alpha version of a contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
 
 import SEO from '../components/general/SEO'
+
+const Wrapper = styled.div`
+  height: 90vh;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  padding: 2.5rem 1.5rem;
+`
+
+const H1 = styled.h1`
+  font-family: ${props => props.theme.fonts.body};
+  font-size: 1rem;
+
+  @media screen and (min-width: ${props => props.theme.responsive.md}) {
+    font-size: 1.5rem;
+  }
+  @media screen and (min-width: ${props => props.theme.responsive.lg}) {
+    font-size: 2rem;
+  }
+`
+
+const StyledLink = styled.a`
+  /* text-decoration: none; */
+  color: ${props => props.theme.colors.text};
+  transition: color 0.3s;
+  :hover {
+    color: ${props => props.theme.colors.hover};
+  }
+`
 
 const Contact = () => (
   <>
     <SEO title="Contact" />
-    <h1>Hi from the Contact page</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <Wrapper>
+      <H1>
+        This will eventually be a form 💌. Until then{' '}
+        <StyledLink href="mailto:sstrubberg@protonmail.com">
+          send me note
+        </StyledLink>
+        .
+      </H1>
+    </Wrapper>
   </>
 )
 
